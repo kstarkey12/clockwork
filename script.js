c25f1e3f99db193cf2232bed0ea0c34b0c56a586
konnor starkey <130928028+kstarkey12@users.noreply.github.com>
2023-05-24 22:32:04 -0400

    script v2

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+// Function to display the current day
+function displayCurrentDay() {
+  var currentDate = new Date();
+  var options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
+  var formattedDate = currentDate.toLocaleDateString('en-US', options);
+  document.getElementById("currentDay").textContent = formattedDate;
+}
+
+// Function to generate time blocks
+function generateTimeBlocks() {
+  var currentTime = new Date().getHours();
+
+  for (var hour = 9; hour <= 17; hour++) {
+    var timeBlock = document.createElement("div");
+    timeBlock.classList.add("time-block");
+
+    var hourText = document.createElement("div");
+    hourText.classList.add("hour");
+    hourText.textContent = formatHour(hour);
+
+    var description = document.createElement("textarea");
+    description.classList.add("description");
+    description.value = getEventFromLocalStorage(hour);
+
+    var saveBtn = document.createElement("button");
+    saveBtn.classList.add("saveBtn");
+    saveBtn.innerHTML = '<i class="fas fa-save"></i>';
+    saveBtn.addEventListener("click", saveEventToLocalStorage.bind(null, hour));
+
+    if (hour < currentTime) {
+      timeBlock.classList.add("past");
+    } else if (hour === currentTime) {
+      timeBlock.classList.add("present");
+    } else {
+      timeBlock.classList.add("future");
+    }
+
+    timeBlock.appendChild(hourText);
+    timeBlock.appendChild(description);
+    timeBlock.appendChild(saveBtn);
+
+    document.getElementById("timeBlocks").appendChild(timeBlock);
+  }
+}
+
+// Helper function to format the hour
+function formatHour(hour) {
+  var suffix = hour >= 12 ? "PM" : "AM";
+  var formattedHour = hour % 12 || 12;
+  return formattedHour + suffix;
+}
+
+// Function to get event from local storage
+function getEventFromLocalStorage(hour) {
+  return localStorage.getItem("event_" + hour) || "";
+}
+
+// Function to save event to local storage
+function saveEventToLocalStorage(hour) {
+  var description = document.querySelector("#timeBlocks #hour-" + hour + " .description").value;
+  localStorage.setItem("event_" + hour, description);
+}
+
+// Call the necessary functions when the page loads
+displayCurrentDay();
+generateTimeBlocks();
